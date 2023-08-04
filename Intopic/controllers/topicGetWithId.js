1d0d3b0d457c83182bb3dafe93625a21098ff29e
Senthil Nathan Kathiresan <>
2023-08-04 09:04:29 -0400

    topic get by id name change

--- Intopic/controllers/topicGetWithId.js
+++ Intopic/controllers/topicGetWithId.js
@@ -4,13 +4,13 @@ const Quiz = require("../models/Quiz.js")
 module.exports = async (req, res) => {
     const topicId = req.params.topicId;
     console.log("TopicWithIDGetPage, param topicid -", topicId, ", UID -> ", req.session.userId);
-    const topics = await Topics.find({_id: topicId, userId: req.session.userId});
-    // console.log("Topic with ID obj -> ", topics);
+    const topic = await Topics.find({_id: topicId, userId: req.session.userId});
+    // console.log("Topic with ID obj -> ", topic);
     const quizzes = await Quiz.find({topicId: topicId, userId: req.session.userId})
     console.log("Quizzes with ID obj -> ", quizzes);
     res.status(200).send({
         "message": "Retrieved", 
-        topics: topics,
+        topic: topic,
         quizzes: quizzes
     });
 }
