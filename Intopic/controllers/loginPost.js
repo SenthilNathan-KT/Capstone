6e49c0bfe18e4e8408098da4b82238e48eddd0af
Senthil Nathan Kathiresan <>
2023-08-12 21:58:20 -0400

    Quiz Post code

--- Intopic/controllers/loginPost.js
+++ Intopic/controllers/loginPost.js
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
             userObj.userId = user._id;
             userObj.email= user.email;
             userObj.userName= user.userName;
-            userObj.token = jwt.sign(userObj, process.env.JSON_WEB_TOKEN_KEY, { expiresIn: "1h" });
+            userObj.token = jwt.sign(userObj, process.env.JSON_WEB_TOKEN_KEY, { expiresIn: "2h" });
             // console.log("User obj after token " + user);
             // console.log("token " + userObj.token);
         } else {
